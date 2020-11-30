f94b6f742ae57ce2536a2d86dfa4ecbdb6589436
adsoftsito <adsoft@live.com.mx>
2020-11-30 08:38:15 +0000

    sample flowers

--- keras/flowers/flowers.py
+++ keras/flowers/flowers.py
@@ -28,4 +28,37 @@ roses = list(data_dir.glob('roses/*'))
 im1 = PIL.Image.open(str(roses[0]))
 im1 = im1.save("rose0.jpg") 
 
+batch_size = 32
+img_height = 180
+img_width = 180
 
+train_ds = tf.keras.preprocessing.image_dataset_from_directory(
+  data_dir,
+  validation_split=0.2,
+  subset="training",
+  seed=123,
+  image_size=(img_height, img_width),
+  batch_size=batch_size)
+
+val_ds = tf.keras.preprocessing.image_dataset_from_directory(
+  data_dir,
+  validation_split=0.2,
+  subset="validation",
+  seed=123,
+  image_size=(img_height, img_width),
+  batch_size=batch_size)
+
+class_names = train_ds.class_names
+print(class_names)
+
+import matplotlib.pyplot as plt
+
+plt.figure(figsize=(10, 10))
+for images, labels in train_ds.take(1):
+  for i in range(9):
+    ax = plt.subplot(3, 3, i + 1)
+    plt.imshow(images[i].numpy().astype("uint8"))
+    plt.title(class_names[labels[i]])
+    plt.axis("off")
+    flowername = class_names[labels[i]]
+    plt.savefig(flowername)
